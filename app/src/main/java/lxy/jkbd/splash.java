df8be8b54e591368de3bcb489522b89d6832d59d
lixingyuan <979728685@qq.com>
2017-06-27 22:20:39 +0800

    添加splasd.java

---
+++ app/src/main/java/lxy/jkbd/splash.java
@@ -0,0 +1,30 @@
+package lxy.jkbd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class splash  extends AppCompatActivity {
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        m.start();
+    }
+    CountDownTimer m = new CountDownTimer(2000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(splash.this,MainActivity.class);
+            startActivity(intent);
+        }
+    };
+}
