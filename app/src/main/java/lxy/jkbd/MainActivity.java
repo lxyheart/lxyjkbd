b209bb7dcaf6eb678ce00b14da3cc91284fea490
lixingyuan <979728685@qq.com>
2017-06-27 23:14:17 +0800

    阻止返回

--- app/src/main/java/lxy/jkbd/MainActivity.java
+++ app/src/main/java/lxy/jkbd/MainActivity.java
@@ -1,13 +1,25 @@
 package lxy.jkbd;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        setResult(RESULT_OK,intent);
+        finish();
+
     }
 }
