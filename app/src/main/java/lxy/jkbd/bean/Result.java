e33972dff76ddc20d25d0e6517f2ec0be73a5f78
lixingyuan <979728685@qq.com>
2017-06-28 20:16:28 +0800

    完成一个Result类

---
+++ app/src/main/java/lxy/jkbd/bean/Result.java
@@ -0,0 +1,19 @@
+package lxy.jkbd.bean;
+
+import java.util.List;
+
+/**
+ * Created by acer on 2017/6/28.
+ */
+
+public class Result {
+    public List<ExamInfo> examinfo;
+
+    public List<ExamInfo> getExaminfo() {
+        return examinfo;
+    }
+
+    public void setExaminfo(List<ExamInfo> examinfo) {
+        this.examinfo = examinfo;
+    }
+}
