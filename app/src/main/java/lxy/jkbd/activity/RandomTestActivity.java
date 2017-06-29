f44754d05f145d81a3188f1f418cddc0c59a4c82
lxyheart <979728685@qq.com>
2017-06-29 10:27:04 +0800

    添加RandomTestActivity.java

---
+++ app/src/main/java/lxy/jkbd/activity/RandomTestActivity.java
@@ -0,0 +1,19 @@
+package lxy.jkbd.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import lxy.jkbd.R;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class RandomTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
