0b8596d9fd56f6f9b16ec2dcae1ecdb3e6466f25
lixingyuan <979728685@qq.com>
2017-06-29 16:31:57 +0800

    添加OkHttpUitls.java类及加载相应的依赖，和配置网络请求权限

--- app/src/main/java/lxy/jkbd/activity/MainActivity.java
+++ app/src/main/java/lxy/jkbd/activity/MainActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 
 import lxy.jkbd.R;
+import lxy.jkbd.bean.ExamInfo;
+import lxy.jkbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void RandomTest(View view) {
+        OkHttpUtils<ExamInfo> utils = new OkHttpUtils<>(getApplicationContext());
+        String url = "http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(url)
+                .targetClass(ExamInfo.class)
+                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>(){
+
+                    @Override
+                    public void onSuccess(ExamInfo result) {
+                        Log.e("main","result="+result);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });
+
         Intent intent = new Intent(MainActivity.this,RandomTestActivity.class);
         startActivity(intent);
     }
