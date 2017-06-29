0a2c97ab8f4e50cd3e8323971d6a27d51ebbb777
lxyheart <979728685@qq.com>
2017-06-29 10:27:42 +0800

    添加RandomTest方法

--- app/src/main/java/lxy/jkbd/activity/MainActivity.java
+++ app/src/main/java/lxy/jkbd/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 
 import lxy.jkbd.R;
 
@@ -17,4 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void RandomTest(View view) {
+        Intent intent = new Intent(MainActivity.this,RandomTestActivity.class);
+        startActivity(intent);
+    }
 }
