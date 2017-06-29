38b37004dd2af8739380906aef776bafdaee2b1b
lixingyuan <979728685@qq.com>
2017-06-29 15:52:00 +0800

    添加退出事件和使用tools隐藏数据

--- app/src/main/java/lxy/jkbd/activity/MainActivity.java
+++ app/src/main/java/lxy/jkbd/activity/MainActivity.java
@@ -22,4 +22,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this,RandomTestActivity.class);
         startActivity(intent);
     }
+
+    public void exit(View view) {
+        finish();
+    }
 }
