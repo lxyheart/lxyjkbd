5bb374e3435de70da7f54bc4bc20bb011ce0e3fc
lixingyuan <979728685@qq.com>
2017-06-30 15:32:27 +0800

    使用Picasso加载图片并显示

--- app/src/main/java/lxy/jkbd/activity/RandomTestActivity.java
+++ app/src/main/java/lxy/jkbd/activity/RandomTestActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.io.Serializable;
 import java.util.List;
@@ -21,6 +24,7 @@ import lxy.jkbd.bean.Question;
 
 public class RandomTestActivity extends AppCompatActivity {
     TextView tvExamInfo,tvExamTitle,tvop1,tvop2,tvop3,tvop4;
+    ImageView imageView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +48,8 @@ public class RandomTestActivity extends AppCompatActivity {
         tvop2 = (TextView)findViewById(R.id.tv_item2);
         tvop3 = (TextView)findViewById(R.id.tv_item3);
         tvop4 = (TextView)findViewById(R.id.tv_item4);
+        imageView = (ImageView)findViewById(R.id.image_title);
+
 
     }
 
@@ -67,6 +73,11 @@ public class RandomTestActivity extends AppCompatActivity {
             tvop2.setText(question.getItem2());
             tvop3.setText(question.getItem3());
             tvop4.setText(question.getItem4());
+            Picasso.with(RandomTestActivity.this)
+                    .load(question.getUrl())
+                    .into(imageView);
+
+
         }
     }
 
